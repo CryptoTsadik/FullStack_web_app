146b479cf24a3cdb110fe2ef286870285fb596f3
CryptoTsadik <arthur.gulko@gmail.com>
2022-12-27 11:51:43 +0100

    typo in populate_price

--- popoulate_prices.py
+++ popoulate_prices.py
@@ -55,4 +55,4 @@ for pair_id, list_of_prices in price_values.items():
                        (pair_id, open_time, open, high, low, close, volume, quote_asset_volume, number_of_trades,
                         taker_buy_base_asset_volume, taker_buy_quote_asset_volume))
 
-# connection.commit()
+connection.commit()
