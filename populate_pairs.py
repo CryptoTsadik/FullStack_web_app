c68d2da7297658635b3da9eccb7d6a48334c4e20
CryptoTsadik <arthur.gulko@gmail.com>
2023-01-06 16:58:24 +0100

    Removal of delisted pairs

--- populate_pairs.py
+++ populate_pairs.py
@@ -1,8 +1,10 @@
 import sqlite3
 from binance import Client
+from config import DB_FILE
+
 
 # Create connection to DB
-connection = sqlite3.connect("app.db")
+connection = sqlite3.connect(DB_FILE)
 
 connection.row_factory = sqlite3.Row
 
@@ -15,7 +17,6 @@ cursor.execute("""
 rows = cursor.fetchall()
 pairs_in_db = [row["pair"] for row in rows]
 
-
 # Create connection to Binance API
 client = Client()
 
@@ -24,10 +25,13 @@ info = client.get_exchange_info()
 
 # Get data only from symbols dict
 symbols = info["symbols"]
+
 exchange = "BINANCE"
 number_of_added_pairs = 0
+number_of_delisted_pairs = 0
 
-# Iterate over list of symbols, check if they are in DB, and add if not
+# Iterate over list of symbols, check if they are in DB, and add if not pairs pair is added to DB.
+# If pair is delisted from exchange symbol and connected data are deleted from DB.
 for symbol in symbols:
     try:
         if symbol["status"] == "TRADING" and symbol["symbol"] not in pairs_in_db:
@@ -35,11 +39,17 @@ for symbol in symbols:
             number_of_added_pairs += 1
             cursor.execute("INSERT INTO currencies (pair, base_asset, quote_asset, exchange) VALUES (?, ?, ?, ?)",
                            (symbol["symbol"], symbol["baseAsset"], symbol["quoteAsset"], exchange))
+        elif symbol["status"] == "BREAK" and symbol["symbol"] in pairs_in_db:
+            print(f"Exchange delisted a pair: {symbol['symbol']}")
+            number_of_delisted_pairs += 1
+            cursor.execute("""DELETE FROM currencies WHERE pair = ?""", (symbol["symbol"],))
     except Exception as e:
         print(symbol["symbol"])
         print(e)
 
 if number_of_added_pairs == 0:
-    print("No new pairs listed on exchange")
+    print("No new pairs were listed on exchange")
+if number_of_delisted_pairs == 0:
+    print("No pairs were delisted from exchange")
 
 connection.commit()
