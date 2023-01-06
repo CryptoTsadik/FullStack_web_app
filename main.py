8b32dba6e2366b374b511023c4745967b8a2dfe2
CryptoTsadik <arthur.gulko@gmail.com>
2023-01-06 17:15:55 +0100

    added filter of pairs

--- main.py
+++ main.py
@@ -3,43 +3,45 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import RedirectResponse
 from fastapi.templating import Jinja2Templates
 from datetime import datetime, date, time
+from config import DB_FILE
+
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-year_today = date.today().year
-month_interval_start = date.today().month
-month_interval_end = date.today().month - 1
+# year_today = date.today().year
+# month_interval_start = date.today().month
+# month_interval_end = date.today().month - 1
 day_today = date.today().day
-day_start = datetime(year=year_today, month=month_interval_end, day=day_today)
+# day_start = datetime(year=year_today, month=month_interval_end, day=day_today)
 
 
 @app.get("/")
 def index(request: Request):
     pairs_filter = request.query_params.get("filter", False)
-    connection = sqlite3.connect("app.db")
+    connection = sqlite3.connect(DB_FILE)
     connection.row_factory = sqlite3.Row
     cursor = connection.cursor()
 
-    if pairs_filter == "Intraday price high (close)":
+    if pairs_filter == "BTC pairs":
         cursor.execute("""
-        SELECT * FROM (
-            SELECT pair, pair_id, max(close), open_time
-            FROM pairs_price
-            JOIN currencies on currencies.id = pairs_price.pair_id
-            GROUP BY pair_id
-            ORDER BY pair
-        ) WHERE open_time > ? """, (day_today,))
+        SELECT *
+        FROM currencies
+        WHERE quote_asset = 'BTC';
+        """)
 
-    if pairs_filter == "Intraday price low (close)":
+    elif pairs_filter == "USDT pairs":
         cursor.execute("""
-        SELECT * FROM (
-            SELECT pair, pair_id, min(close), open_time
-            FROM pairs_price
-            JOIN currencies on currencies.id = pairs_price.pair_id
-            GROUP BY pair_id
-            ORDER BY pair
-        ) WHERE open_time > ? """, (day_today,))
+        SELECT *
+        FROM currencies
+        WHERE quote_asset = 'USDT';
+        """)
+    elif pairs_filter == "ETH pairs":
+        cursor.execute("""
+        SELECT *
+        FROM currencies
+        WHERE quote_asset = 'ETH';
+        """)
     else:
         cursor.execute("""
                     SELECT pair, base_asset, quote_asset FROM currencies ORDER BY pair 
@@ -53,7 +55,7 @@ def index(request: Request):
 @app.get("/pair/{pair}")
 def pair_detail(request: Request, pair):
     # Create connection to DB
-    connection = sqlite3.connect("app.db")
+    connection = sqlite3.connect(DB_FILE)
     connection.row_factory = sqlite3.Row
     cursor = connection.cursor()
 
@@ -80,7 +82,7 @@ def pair_detail(request: Request, pair):
 
 @app.post("/apply_strategy")
 def apply_strategy(strategy_id: int = Form(...), pair_id: int = Form(...)):
-    connection = sqlite3.connect("app.db")
+    connection = sqlite3.connect(DB_FILE)
     cursor = connection.cursor()
 
     cursor.execute("""
@@ -93,7 +95,7 @@ def apply_strategy(strategy_id: int = Form(...), pair_id: int = Form(...)):
 
 @app.get("/strategy/{strategy_id}")
 def strategy(request: Request, strategy_id):
-    connection = sqlite3.connect("app.db")
+    connection = sqlite3.connect(DB_FILE)
     connection.row_factory = sqlite3.Row
 
     cursor = connection.cursor()
